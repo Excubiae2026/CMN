c2384d192662235947111325a681e9ccd0099747
Excubiae <excubiae2026@hotmail.com>
2025-11-26 22:20:40 -0500

    Delete client directory

--- client/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from gui import NodeGUI
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = NodeGUI()
-    window.show()
-    sys.exit(app.exec())
